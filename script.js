c8c662b84508ea42c794137d413a346188542df8
Marco <marco.palummieri@gmail.com>
2025-09-15 16:32:40 +0200

    Refactor toggle comments for clarity and standardize quotes in JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-function darkMode(){
-    document.body.classList.toggle('dark-mode');
+function darkMode() {
+  document.body.classList.toggle("dark-mode");
 }
 
-const toggle = document.getElementById('darkModeToggle');
-toggle.addEventListener('change', darkMode);
+const toggle = document.getElementById("darkModeToggle");
+toggle.addEventListener("change", darkMode);
 
-const DmToggle = document.getElementById('dm-toggle');
-DmToggle.addEventListener('change', darkMode);
+const DmToggle = document.getElementById("dm-toggle");
+DmToggle.addEventListener("change", darkMode);
 
-const secondToggle = document.getElementById('second-toggle');
-secondToggle.addEventListener('change', darkMode);
+const secondToggle = document.getElementById("second-toggle");
+secondToggle.addEventListener("change", darkMode);
