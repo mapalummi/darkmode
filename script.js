a38141045198fa25e99117f932b417ebf30360ed
Marco <marco.palummieri@gmail.com>
2025-09-15 16:16:54 +0200

    Add second dark mode toggle with SVG icons and styling

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ toggle.addEventListener('change', darkMode);
 
 const DmToggle = document.getElementById('dm-toggle');
 DmToggle.addEventListener('change', darkMode);
+
+const secondToggle = document.getElementById('second-toggle');
+secondToggle.addEventListener('change', darkMode);
