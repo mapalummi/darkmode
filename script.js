147eff357ed7951b206780d9b0c37f634b04502a
Marco <marco.palummieri@gmail.com>
2025-09-15 16:56:20 +0200

    Refactor comments for clarity and update YT toggle switch label description

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ function darkMode() {
   document.body.classList.toggle("dark-mode");
 }
 
+// Einfacher Slider
 const toggle = document.getElementById("darkModeToggle");
 toggle.addEventListener("change", darkMode);
 
+// Slider mit SVG Icons
 const DmToggle = document.getElementById("dm-toggle");
 DmToggle.addEventListener("change", darkMode);
 
